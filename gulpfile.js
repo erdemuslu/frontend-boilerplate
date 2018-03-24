a05ff7dfdf43acf2d7652370ad5f854132b2a16e
Erdem Uslu <erdemuslu@Erdems-MacBook-Pro.local>
2018-03-24 21:30:19 +0300

    browser-sync added

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,30 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync'),
+    reload = browserSync.reload();
+
+var sourcePath = {
+    sassSource: 'src/scss/*.scss'
+}
+
+var appPath = {
+    root: 'app/',
+    css: 'app/css',
+    js: 'app/js'
+}
 
 gulp.task('sass', function() {
-    return gulp.src('src/scss/app.scss')
+    return gulp.src(sourcePath.sassSource)
         .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
-        .pipe(gulp.dest('app/css'));
+        .pipe(gulp.dest(appPath.css));
 });
 
-gulp.task('default', ['sass']);
+gulp.task('serve', ['sass'], function() {
+    browserSync.init([appPath.css + '/*.css', appPath.root + '/*.html', appPath.js + '/*.js'], {
+        server:{
+            baseDir: appPath.root
+        }
+    })
+});
+
+gulp.task('default', ['serve']);
