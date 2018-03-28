01f5e2893fb17f88c49feadc06097ee0dc329472
Erdem Uslu <erdem@mobilex.com.tr>
2018-03-28 16:50:37 +0300

    lodash added

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,12 @@
-var $ = require('jquery');
+var $ = require('jquery'),
+    _ = require('lodash');
 
 $(document).ready(() => {
 
-    
-
+    _([1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 1, 2, 3, 4, 5, 6, 1, 2, 3, 3, 1])
+        .uniq()
+        .each(function (i) {
+            console.log(i);
+        });
+ 
 });
