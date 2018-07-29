4a6f8d7e7b20cb63de43db2fab9d60419df9bdab
Erdem <usluerdem@yandex.com>
2018-07-29 15:00:56 +0300

    add move method into gulpfile & change attachment folder name as assets

--- gulpfile.js
+++ gulpfile.js
@@ -1,95 +1,94 @@
-var gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    browserSync = require('browser-sync'),
-    autoprefixer = require('gulp-autoprefixer'),
-    clean = require('gulp-clean'),
-    concat = require('gulp-concat'),
-    merge = require('merge-stream'),
-    minify = require('gulp-minify-css'),
-    newer = require('gulp-newer'),
-    imagemin = require('gulp-imagemin'),
-    pug = require('gulp-pug'),
-    babelify = require('babelify'),
-    browserify = require('browserify'),
-    source = require('vinyl-source-stream'),
-    buffer = require('vinyl-buffer'),
-    sourcemaps = require('gulp-sourcemaps'),
-    connect = require('gulp-connect');
+var gulp = require("gulp"),
+    sass = require("gulp-sass"),
+    browserSync = require("browser-sync"),
+    autoprefixer = require("gulp-autoprefixer"),
+    clean = require("gulp-clean"),
+    concat = require("gulp-concat"),
+    merge = require("merge-stream"),
+    minify = require("gulp-minify-css"),
+    newer = require("gulp-newer"),
+    imagemin = require("gulp-imagemin"),
+    pug = require("gulp-pug"),
+    babelify = require("babelify"),
+    browserify = require("browserify"),
+    source = require("vinyl-source-stream"),
+    buffer = require("vinyl-buffer"),
+    sourcemaps = require("gulp-sourcemaps"),
+    connect = require("gulp-connect");
 
 var sourcePath = {
-    sassSource: 'src/scss/**/*',
-    jsSource: 'src/js/**/*',
-    imgSource: 'src/img/**/*',
-    pugSource: 'src/views/*.pug',
-    attachmentsSource: 'src/attachments/**/*'
-}
+    sassSource: "src/scss/**/*",
+    jsSource: "src/js/**/*",
+    imgSource: "src/img/**/*",
+    pugSource: "src/views/*.pug",
+    fontSource: "src/fonts/**/*",
+    attachmentsSource: "src/assets/**/*"
+};
 
 var appPath = {
-    root: 'app/',
-    css: 'app/css',
-    js: 'app/js',
-    img: 'app/img',
-    attachmentsSource: 'app/'
-}
+    root: "app/",
+    css: "app/css",
+    js: "app/js",
+    img: "app/img",
+    attachmentsSource: "app/"
+};
 
-gulp.task('clean-script', function() {
-    return gulp.src(appPath.js + '/*.js', {read: false, force: true})
+gulp.task("clean-script", function() {
+    return gulp.src(appPath.js + "/*.js", {read: false, force: true})
         .pipe(clean());
 });
 
-gulp.task('clean-html', function() {
-    return gulp.src(appPath.root + '/*.html', {read: false, force: true})
+gulp.task("clean-html", function() {
+    return gulp.src(appPath.root + "/*.html", {read: false, force: true})
         .pipe(clean());
 });
 
-gulp.task('clean-css', function() {
-    return gulp.src(appPath.css + '/*.css', {read: false, force: true})
+gulp.task("clean-css", function() {
+    return gulp.src(appPath.css + "/*.css", {read: false, force: true})
         .pipe(clean());
 });
 
-gulp.task('views', ['clean-html'], function () {
+gulp.task("views", ["clean-html"], function () {
     return gulp.src(sourcePath.pugSource)
         .pipe(pug({
-            doctype: 'html',
+            doctype: "html",
             pretty: false
         }))
-        .pipe(gulp.dest('./app/'));
+        .pipe(gulp.dest("./app/"));
 });
 
-gulp.task('script', function(){  
+gulp.task("script", function(){  
     return browserify({
-        entries: ['./src/js/script.js']
+        entries: ["./src/js/script.js"]
     })
     .transform(babelify.configure({
         presets: ["env"]
     }))
     .bundle()
-    .pipe(source('bundle.js'))
+    .pipe(source("bundle.js"))
     .pipe(buffer())
     .pipe(sourcemaps.init({loadMaps: true}))
-    .pipe(sourcemaps.write('./'))
+    .pipe(sourcemaps.write("./"))
     .pipe(gulp.dest(appPath.js))
     .pipe(connect.reload());
 });
 
-gulp.task('sass', function () {
+gulp.task("sass", function () {
 
     var cssStream = gulp.src(
-        ['./node_modules/slick-carousel/slick/slick.css',
-        './bower_components/normalize.css/normalize.css',
-        './bower_components/bootstrap/dist/css/bootstrap.min.css',
-        './bower_components/Ionicons/css/ionicons.min.css',])
-        .pipe(concat('css-files.css'));
+        [
+            "./bower_components/tiny-slider/dist/tiny-slider.css"
+        ])
+        .pipe(concat("css-files.css"));
 
     var scssStream = gulp.src([sourcePath.sassSource])
         .pipe(sass())
-        .pipe(concat('scss-files.scss'));
-
+        .pipe(concat("scss-files.scss"));
 
     var mergedStream = merge(cssStream, scssStream)
-        .pipe(concat('style.css'))
+        .pipe(concat("style.css"))
         .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
+            browsers: ["last 2 versions"],
             cascade: false
         }))
         .pipe(minify())
@@ -99,25 +98,26 @@ gulp.task('sass', function () {
 
 });
 
-gulp.task('images', function () {
+gulp.task("images", function () {
     return gulp.src(sourcePath.imgSource)
         .pipe(newer(appPath.img))
         .pipe(imagemin())
         .pipe(gulp.dest(appPath.img));
 });
 
-gulp.task('move', function () {
+gulp.task("move", function () {
     return gulp.src(sourcePath.attachmentsSource)
         .pipe(gulp.dest(appPath.attachmentsSource));
 });
 
-gulp.task('setup', ['sass', 'images', 'views', 'script', 'move']);
+gulp.task("setup", ["sass", "images", "views", "script", "move"]);
 
-gulp.task('watch', function () {
-    gulp.watch(sourcePath.sassSource, ['sass']);
-    gulp.watch(['src/views/**/*', sourcePath.pugSource], ['views']);
-    gulp.watch(sourcePath.jsSource, ['script']);
-    gulp.watch(sourcePath.imgSource, ['images']);
+gulp.task("watch", function () {
+    gulp.watch(sourcePath.sassSource, ["sass"]);
+    gulp.watch(["src/views/**/*", sourcePath.pugSource], ["views"]);
+    gulp.watch(sourcePath.jsSource, ["script"]);
+    gulp.watch(sourcePath.imgSource, ["images"]);
+    gulp.watch(sourcePath.attachmentsSource, ["move"]);
 
     // init server
     browserSync.init({
@@ -127,7 +127,7 @@ gulp.task('watch', function () {
         }
     });
 
-    gulp.watch([appPath.root + '**'], browserSync.reload);
+    gulp.watch([appPath.root + "**"], browserSync.reload);
 });
 
-gulp.task('default', ['watch']);
+gulp.task("default", ["watch"]);
